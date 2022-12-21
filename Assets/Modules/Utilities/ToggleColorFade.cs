56cd5bc9448c3d111376b1a0a51888c7b637560d
dogdie233 <dogdie233@163.com>
2022-12-21 19:48:39 +0800

    feat: 给MainMenu的TabBar添加了切换过渡动画

---
+++ Assets/Modules/Utilities/ToggleColorFade.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Klrohias.NFast.Utilities
+{
+    [RequireComponent(typeof(Toggle))]
+    public class ToggleColorFade : MonoBehaviour
+    {
+        [SerializeField] private Graphic[] graphics;
+        [SerializeField] private Color offColor;
+        [SerializeField] private Color onColor;
+        [SerializeField] private float duration;
+
+        private void Awake()
+        {
+            var toggle = GetComponent<Toggle>();
+            foreach (Graphic graphic in graphics)
+                graphic.canvasRenderer.SetColor(toggle.isOn ? onColor : offColor);
+            toggle.onValueChanged.AddListener(OnToggleValueChanged);
+        }
+
+        private void OnToggleValueChanged(bool isOn)
+        {
+            foreach (Graphic graphic in graphics)
+                graphic.CrossFadeColor(isOn ? onColor : offColor, duration, true, true);
+        }
+    }
+}
