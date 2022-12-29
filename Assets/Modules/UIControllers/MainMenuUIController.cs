351f88c65b05fddc4a7dd4aa861269f0557f63e8
qingyiwebt <qingyiweb@gmail.com>
2022-12-29 21:56:22 +0800

    Add chart list in `MainMenuScene`

--- Assets/Modules/UIControllers/MainMenuUIController.cs
+++ Assets/Modules/UIControllers/MainMenuUIController.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Klrohias.NFast.Native;
 using Klrohias.NFast.Navigation;
 using Klrohias.NFast.PhiGamePlay;
+using Klrohias.NFast.UIComponent;
 using Klrohias.NFast.Utilities;
 using TMPro;
 using UnityEngine;
@@ -12,7 +13,11 @@ namespace Klrohias.NFast.UIControllers
 {
     public class MainMenuUIController : MonoBehaviour
     {
-        public Button TestButton;
+        [Serializable]
+        public struct HomeProperties
+        {
+            public Transform Viewport;
+        }
         [Serializable]
         public struct LocalImportProperties
         {
@@ -21,10 +26,53 @@ namespace Klrohias.NFast.UIControllers
             public TMP_InputField PathInput;
         }
         public LocalImportProperties LocalImport;
+        public HomeProperties Home;
+        public GameObject CommandButtonPrefab;
+        private ObjectPool _homeListItemPool;
+
+        private void Awake()
+        {
+            _homeListItemPool = new ObjectPool(() =>
+            {
+                var item = Instantiate(CommandButtonPrefab);
+                var transform = item.transform;
+                transform.SetParent(Home.Viewport, false);
+                transform.localScale = Vector3.one;
+                transform.GetComponent<CommandButton>().OnClick += OnHomeListItemSelected;
+                return item;
+            }, 10);
+        }
+
+        private void OnHomeListItemSelected(object obj)
+        {
+            var typedParam = obj as string;
+            if (typedParam == null) return;
+            var navSrv = NavigationService.Get();
+            navSrv.ExtraData = typedParam;
+            navSrv.LoadScene("Scenes/PhiPlayScene");
+        }
+
         private void Start()
         {
             LocalImport.BrowseButton.onClick.AddListener(BrowseImportFile);
             LocalImport.ImportButton.onClick.AddListener(ImportFile);
+            UpdateCharts();
+        }
+
+        private void UpdateCharts()
+        {
+            _homeListItemPool.ReturnAll();
+
+            var chartPath = OSService.Get().ChartPath;
+            var files = Directory.GetFiles(chartPath, "*.nfp");
+            foreach (var file in files)
+            {
+                var item = _homeListItemPool.RequestObject();
+                var commandButton = item.GetComponent<CommandButton>();
+                commandButton.CommandValue = file;
+                commandButton.Title = Path.GetFileName(file);
+                item.SetActive(true);
+            }
         }
 
         private async void ImportFile()
